7b95ffc3be912bc94221a096e906f60852d35fa0
n0k0m3 <10925298+n0k0m3@users.noreply.github.com>
2024-01-24 16:59:52 -0500

    feat(data): classifier: using original paper data

--- data/classifier_gen.py
+++ data/classifier_gen.py
@@ -2,28 +2,26 @@ import os
 os.chdir(os.path.dirname(os.path.realpath(__file__)))
 
 
-# download archanatikayatray/aeroBERT-classification huggingface dataset
-# https://huggingface.co/datasets/archanatikayatray/aeroBERT-classification
+# use dataset split given from the author of the paper for classification task
 import pandas as pd
 from datasets import load_dataset, Dataset
 
 if __name__ == "__main__":
-    dataset = load_dataset("archanatikayatray/aeroBERT-classification")["train"]["text"]
-    # load dataset as pandas dataframe, header is first line, delimiter is *
-    df = pd.DataFrame([l.split("*") for l in dataset[1:]], columns=dataset[0].split("*"))
-
+    train_df = pd.read_csv("./classifier/train_data_classification.csv")
+    train_df.columns = ["requirements","label"]
+    test_df = pd.read_csv("./classifier/test_data_classification.csv")
+    test_df.columns = ["requirements","label"]
     # convert integer labels to string labels
     # Design requirements: 0
     # Functional requirements: 1
     # Performance requirements: 2
-    df["label"] = df["label"].map({"0": "Design", "1": "Functional", "2": "Performance"})
+    train_df["label"] = train_df["label"].map({0: "Design", 1: "Functional", 2: "Performance"})
+    test_df["label"] = test_df["label"].map({0: "Design", 1: "Functional", 2: "Performance"})
 
     # make sure df is in correct format for hf datasets
-    dataset = Dataset.from_pandas(df)
-
-    # train/test split
-    data = dataset.train_test_split(test_size=0.1, seed=42)
+    train = Dataset.from_pandas(train_df)
+    test = Dataset.from_pandas(test_df)
 
     # save to file
-    data["train"].to_json("./classifier/train.json")
-    data["test"].to_json("./classifier/test.json")
+    train.to_json("./classifier/train.json")
+    test.to_json("./classifier/test.json")
